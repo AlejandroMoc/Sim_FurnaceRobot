37b70f0fb665a7ae13c371840cc14e1566059806
Daniel <a01736353@tec.mx>
2023-08-21 12:04:00 -0600

    Actualizado modelo de ejemplo.

--- pacman.py
+++ pacman.py
@@ -5,53 +5,111 @@ from mesa.time import RandomActivation
 from mesa.visualization.modules import CanvasGrid
 from mesa.visualization.ModularVisualization import ModularServer
 import math
+import heapq
 
-#posiciones iniciales de los agentes
-pacmanposition=(8, 7)
-ghostposition1=(10,12)
+#Posiciones iniciales de los agentes
+pacmanposition=(7,7)
+ghostposition1=(1,2)
 
 
 #Funcion distancia
 def distancia_entre_puntos(p1, p2):
     return math.sqrt((p2[0] - p1[0])**2 + (p2[1] - p1[1])**2)
 
+
+#Agregados para A*
+class Node:
+    def __init__(self, position, parent=None):
+        self.position = position
+        self.parent = parent
+        self.g = 0
+        self.h = 0
+        self.f = 0
+
+    def __lt__(self, other):
+        return self.f < other.f
+
+def astar(grid, start, end):
+    open_list = []
+    closed_set = set()
+
+    start_node = Node(start)
+    end_node = Node(end)
+    
+    heapq.heappush(open_list, start_node)
+
+    while open_list:
+        current_node = heapq.heappop(open_list)
+
+        if current_node.position == end_node.position:
+            path = []
+            while current_node:
+                path.append(current_node.position)
+                current_node = current_node.parent
+            return path[::-1]
+        
+        closed_set.add(current_node.position)
+
+        for neighbor in get_neighbors(current_node.position, grid):
+            if neighbor in closed_set:
+                continue
+            
+            g_score = current_node.g + 1
+            h_score = heuristic(neighbor, end_node.position)
+            f_score = g_score + h_score
+
+            if any(neighbor == node.position for node in open_list):
+                existing_node = next(node for node in open_list if node.position == neighbor)
+                if g_score < existing_node.g:
+                    existing_node.g = g_score
+                    existing_node.f = f_score
+                    existing_node.parent = current_node
+            else:
+                neighbor_node = Node(neighbor, current_node)
+                neighbor_node.g = g_score
+                neighbor_node.h = h_score
+                neighbor_node.f = f_score
+                heapq.heappush(open_list, neighbor_node)
+
+    return None
+
+def get_neighbors(position, grid):
+    neighbors = []
+    col, row = position  # Swap col and row here
+    rows, cols = len(grid), len(grid[0])
+
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+    for dc, dr in directions:  # Swap dc and dr here
+        new_col, new_row = col + dc, row + dr  # Swap new_col and new_row here
+        if 0 <= new_row < rows and 0 <= new_col < cols and grid[new_row][new_col] == 1:
+            neighbors.append((new_col, new_row))  # Swap new_col and new_row here
+    
+    return neighbors
+
+def heuristic(position, goal):
+    return abs(position[0] - goal[0]) + abs(position[1] - goal[1])
+
 # Clase fantasma
 class Ghost(Agent):
-    def __init__(self, model, pos, pac):
+    def __init__(self, model, pos, pac, grid):
         super().__init__(model.next_id(), model)
         self.pos = pos
         #Agregado
         self.radio = 1.0
         self.pac = pac
         self.last = pos
-
+        self.grid = grid
+        self.count = 0
+        
     def step(self):
-        
-        #Obtener todos los movimientos posibles
-        next_moves = self.model.grid.get_neighborhood(self.pos, moore=False)
-        
-        #Separar movimientos posibles
-        posiblemovs=[]
-        for movim in next_moves:
-            x, y = movim
-            if self.model.matrix[y][x] == 0:
-                continue
-            else:
-                posiblemovs.append(movim)
-
-        mejor_movimiento = None
-        mejor_distancia = float('inf')  # Inicializar con un valor muy grande
         pacmannode = self.pac.getPos()
-
-        for movim in posiblemovs:
-            distancia = distancia_entre_puntos(movim, pacmannode)
-            if distancia < mejor_distancia and movim != self.last:
-                mejor_distancia = distancia
-                mejor_movimiento = movim
-
-        next_move = mejor_movimiento
-        self.last = self.pos 
-        self.model.grid.move_agent(self, next_move)
+        path = astar(self.grid, ghostposition1,pacmanposition)
+        if self.count < len(path):
+            next_move = path[self.count]
+            print(next_move)
+            self.model.grid.move_agent(self, next_move)          
+            self.count += 1
         
 class Pacman(Agent):
     def __init__(self, model, pos):
@@ -60,7 +118,6 @@ class Pacman(Agent):
         self.radio = 1
 
     def step(self):
-        #Descomentar para que el pacman se mueva aleatoriamente
         """ 
         #Obtener todos los movimientos posibles
         next_moves = self.model.grid.get_neighborhood(self.pos, moore=False)
@@ -81,10 +138,15 @@ class Pacman(Agent):
         """
         return None
         #Agregado
-    #Retorna posicion actual
     def getPos(self):
         return self.pos
         
+    def buscaColision(self, fantasma):
+        dx = self.pos[0] - fantasma.self.pos[0]
+        dy = self.pos[1] - fantasma.self.pos[1]
+        distancia = math.sqrt(dx*dx + dy*dy)
+        return distancia < self.radio + fantasma.self.radio
+
 
 #Clase de piso (blanco)
 class Piso(Agent):
@@ -142,7 +204,7 @@ class Maze(Model):
         self.schedule.add(pacman)
 
         #Spawnear Ghost
-        ghost = Ghost(self, ghostposition1, pacman)
+        ghost = Ghost(self, ghostposition1, pacman, self.matrix)
         self.grid.place_agent(ghost, ghost.pos)
         self.schedule.add(ghost)
 
